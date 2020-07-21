1f9d88c0d31ccdde07c15d9f5f6131e829b09520
kurian <kurian@uno.local>
2020-07-21 12:45:46 +0530

    template

---
+++ index.ts
@@ -0,0 +1,11 @@
+function log(msg){
+  console.log(msg);
+};
+function sum(a,b){
+    return a+b
+}
+function diff(a,b){
+    return (a-b)*(a-b)
+}
+
+export default{log: log, sum: sum, diff: diff };
